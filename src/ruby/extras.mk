aa4209807062d94081bc492f19ca70726468db88
Clement Hussenot <chussenota@gmail.com>
2018-03-18 16:10:41 +0100

    Still in the ruby implem

--- src/ruby/extras.mk
+++ src/ruby/extras.mk
@@ -1,13 +1,14 @@
 WD:=`pwd`
+DIR=${WD}/src/ruby
 
 ruby-install:	## Run ruby tests
-	cd ${WD}/src/ruby && \
+	cd ${DIR} && \
 	bundle install
 
 ruby-test:	## Run ruby tests
-	cd ${WD}/src/ruby && \
+	cd ${DIR} && \
 	bundle exec rspec .
 
 ruby-console:	## Open a PRY console
-	cd ${WD}/src/ruby && \
+	cd ${DIR} && \
 	bundle exec pry -r ./bin/mower
